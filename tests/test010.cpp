acf44bfef83a753d9697f2787df7e0d56f062299
Kristofer Berggren <kristofer.berggren@gmail.com>
2018-03-31 22:54:51 +0800

    follow up to efeed34 fixing tests

--- tests/test010.cpp
+++ tests/test010.cpp
@@ -19,7 +19,7 @@ int main()
 
   try
   {
-    rapidcsv::Document doc(path, rapidcsv::LabelParams(), rapidcsv::SeparatorParams(',', false));
+    rapidcsv::Document doc("", rapidcsv::LabelParams(), rapidcsv::SeparatorParams(',', false));
 
     doc.SetColumn<int>(0, std::vector<int>({2, 3}));
     doc.SetColumn<int>(1, std::vector<int>({4, 9, 16, 25}));
