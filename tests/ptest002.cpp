2c80b5dfb03df15db5c46254daaa1b6af9020785
Kristofer Berggren <kristofer.berggren@gmail.com>
2018-07-08 17:04:43 +0800

    add basic performance tests

---
+++ tests/ptest002.cpp
@@ -0,0 +1,34 @@
+// ptest002.cpp - file load
+
+#include <rapidcsv.h>
+#include "perftest.h"
+#include "unittest.h"
+
+int main()
+{
+  int rv = 0;
+
+  try
+  {
+    perftest::Timer timer;
+
+    for (int i = 0; i < 10; ++i)
+    {
+      timer.Start();
+
+      rapidcsv::Document doc("../tests/msft.csv");
+
+      timer.Stop();
+    }
+    
+    timer.ReportMedian();
+  }
+  catch(const std::exception& ex)
+  {
+    std::cout << ex.what() << std::endl;
+    rv = 1;
+  }
+
+  return rv;
+}
+
